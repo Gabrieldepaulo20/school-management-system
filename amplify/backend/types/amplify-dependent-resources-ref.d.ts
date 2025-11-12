e40ec3a004a2566db80f3352d0ece3c3390c78f5
Gabriel William de Paulo <gabrielwilliam208@gmail.com>
2025-11-12 01:40:17 -0300

    feat: Adiciona backend inicial do Amplify (API e Lambda)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apiPrincipal": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "mainRouterFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
